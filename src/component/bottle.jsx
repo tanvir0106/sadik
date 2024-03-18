8f54c1c3f33014ad06d3c3a64484725da8c9c72b
tanvir0106 <tanvir0106@hotmail.com>
2024-03-18 20:00:33 +0600

    last

--- src/component/bottle.jsx
+++ src/component/bottle.jsx
@@ -1,10 +1,14 @@
 const Bottle = ({bottle}) => {
-    const {recipe_name, recipe_image, recipe_id} = bottle;
+    const {recipe_name, recipe_image, recipe_id,short_description} = bottle;
   return (
   <div>
     <h3>Bottle: {recipe_name}</h3>
     {/* <img src={recipe_image} alt=""/> */}
     <p>Price: ${recipe_id}
+    </p>
+    <p>
+        
+       {short_description}
     </p>
   </div>
   );
